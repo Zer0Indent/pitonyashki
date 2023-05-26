02bde4d8c1970d3ee9467274620970ff454451da
Alex <smi.smi.aleks@yandex.ru>
2023-05-26 18:30:40 +0300

    Laba9

--- Александр/Lab4.py
+++ Александр/Lab4.py
@@ -1,14 +1,7 @@
-a = "*"
-
-top = a * 4
-bot = a * 4
-
-
-
-
-
-
-
-
-print(top)
-print(bot)
+v = int(input())
+t = int(input())
+MKAD = 109
+how = int(v * t) #скок км прошел
+#circle = int(how / MKAD)  #скок кругов
+#point = int(how % MKAD)  #на каком км
+print(int(how))
